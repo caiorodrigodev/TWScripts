4bed2734d18b9b8758f981ee50241c46cc7ef112
Caio <midia@investbankcompany.com.br>
2023-04-03 20:27:33 -0300

    Rename

--- AutoFarm.user.js
+++ AutoFarm.user.js
@@ -17,59 +17,59 @@ var menu = $("#am_widget_Farm a.farm_icon_a");
 var underAttack = $(menu).parent().parent().find("img.tooltip").length + "000";
 
 if (removeUnderAttack == true) {
-  $("img").each(function () {
-    var tempStr = $(this).attr("src");
-    if (tempStr.indexOf("attack") != -1) {
-      $(this).addClass("tooltip");
-    }
-  });
+	$("img").each(function () {
+		var tempStr = $(this).attr("src");
+		if (tempStr.indexOf("attack") != -1) {
+			$(this).addClass("tooltip");
+		}
+	});
 }
 if (refreshPage == true) {
-  setInterval(function () {
-    window.location.reload();
-  }, 60000);
+	setInterval(function () {
+		window.location.reload();
+	}, 60000);
 }
 
 console.log(
-  "Ja existe " +
-    underAttack.substring(0, underAttack.length - 3) +
-    " aldeia com ataque."
+	"Ja existe " +
+	underAttack.substring(0, underAttack.length - 3) +
+	" aldeia com ataque."
 );
 
 var altAldTempo =
-  parseInt($("#am_widget_Farm a.farm_icon_a").length + "00") -
-  parseInt(underAttack);
+	parseInt($("#am_widget_Farm a.farm_icon_a").length + "00") -
+	parseInt(underAttack);
 console.log("Resta " + altAldTempo + " aldeias para Atacar.");
 
 if (altAldTempo == "0") {
-  var altAldTempo = aleatorio(100, 200);
+	var altAldTempo = aleatorio(100, 200);
 } else {
-  var altAldTempo = parseInt(altAldTempo) + parseInt(aleatorio(1000, 1500));
+	var altAldTempo = parseInt(altAldTempo) + parseInt(aleatorio(1000, 1500));
 }
 
 console.log("Resta " + altAldTempo + " milesegundos para alternar a aldeia.");
 
 function aleatorio(inferior, superior) {
-  numPosibilidades = superior - inferior;
-  aleat = Math.random() * numPosibilidades;
-  return Math.round(parseInt(inferior) + aleat);
+	numPosibilidades = superior - inferior;
+	aleat = Math.random() * numPosibilidades;
+	return Math.round(parseInt(inferior) + aleat);
 }
 
 for (i = 0; i < 100; i++) {
-  $(menu)
-    .eq(i)
-    .each(function () {
-      if (!$(this).parent().parent().find("img.tooltip").length) {
-        var tempoAgora = tempo * ++x - aleatorio(160, 320);
-        setTimeout(
-          function (minhaVar) {
-            $(minhaVar).click();
-          },
-          tempoAgora,
-          this
-        );
-      }
-    });
+	$(menu)
+		.eq(i)
+		.each(function () {
+			if (!$(this).parent().parent().find("img.tooltip").length) {
+				var tempoAgora = tempo * ++x - aleatorio(160, 320);
+				setTimeout(
+					function (minhaVar) {
+						$(minhaVar).click();
+					},
+					tempoAgora,
+					this
+				);
+			}
+		});
 }
 
 // function nextPage() {
@@ -77,8 +77,8 @@ for (i = 0; i < 100; i++) {
 // }
 
 function swapVillage() {
-  $(".arrowRight").click();
-  $(".groupRight").click();
+	$(".arrowRight").click();
+	$(".groupRight").click();
 }
 
 setInterval(swapVillage, altAldTempo);
